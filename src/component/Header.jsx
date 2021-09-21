d9f83754e009c3d619002aa06fda1111e675231b
Nizar7zak <nizar7zakout@gmail.com>
2021-09-21 20:44:40 +0300

    finish style for header and pass it on app relates #3

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,13 +1,31 @@
 import React from "react";
+import "./componentCss/Header.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
-      <div>
-        <h3>HOME</h3>
-        <nav></nav>
-      </div>
-    </header>
+    <Router>
+      <header>
+        <ul>
+          <li>
+            <Link to="/">HOME</Link>
+          </li>
+          <li>
+            <Link to="/new">Newest</Link>
+          </li>
+          <li>
+            <Link to="/read">To Read</Link>
+          </li>
+        </ul>
+      </header>
+
+      <Switch>
+        <Route exact path="/"></Route>
+        <Route exact path="/new"></Route>
+        <Route exact path="/read"></Route>
+      </Switch>
+    </Router>
   );
 };
 
