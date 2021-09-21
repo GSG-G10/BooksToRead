afb2444c5e646fc8b13f4714b1822431b93ae9a5
Nizar7zak <nizar7zakout@gmail.com>
2021-09-21 20:10:47 +0300

    insall react react-router-dom to handle header for all pages #relates 3

---
+++ src/component/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h3>HOME</h3>
+        <nav></nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
